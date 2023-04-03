0440f7c37afc2084c22bf21955b6a42e3f662f24
KIMBS <iimkbsu@naver.com>
2023-04-03 15:50:50 +0900

    23.04.03

--- java_algorithm_practice/src/implementation/implementationExample4.java
+++ java_algorithm_practice/src/implementation/implementationExample4.java
@@ -19,22 +19,26 @@ public class implementationExample4 {
 	static void rearrange(String s) {
 		
 		//정규표현식 문자열s에서 0~9까지 문자만 빼서 문자 배열로 저장
-		char num[] = s.replaceAll("[^0-9]", "").toCharArray();
+		char[] num = s.replaceAll("[^0-9]", "").toCharArray();
 		
 		//문자 배열 alpha에 A~Z까지 알파벳 대문자만 빼서 문자배열로 바꾼 후 저장
-		char alpha[] = s.replaceAll("[^A-Z]", "").toCharArray();
+		char[] alpha = s.replaceAll("[^A-Z]", "").toCharArray();
 		Arrays.sort(alpha);
 		
 		int sum = 0;
 		
 		for(char c : num) {
-			int a = c - '0';
+			int a = c - '0'; //숫자 문자를 int형으로 바꾸는 작업
 			sum += a;
 		}
 		
 		String sortedAlpha = new String(alpha);
 		//삼항연산자잖아 ㅋㅋ C언어에서 했던건데 그대로 쓸 수 있구만
-		String addedNum = (sum == 0 ? "" : String.valueOf(sum));
+		//int를 문자열로 바꾸는 방법
+		//Integer.toString(바꿀숫자) //NPE 에러처리
+		//String.valueOf(바꿀숫자) //null로 반환
+		String addedNum = (sum == 0 ? "" : Integer.toString(sum));
+		//String addedNum = (sum == 0?"" : String.valueOf(sum));
 		
 		System.out.println(sortedAlpha + addedNum);
 		
